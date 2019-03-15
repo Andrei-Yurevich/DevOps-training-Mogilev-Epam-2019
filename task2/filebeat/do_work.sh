1414de4f8a4fb7d9d345ebd64f6f06481d864013
vagrant <vagrant@localhost.localdomain>
2019-03-15 16:20:09 +0000

    ¯ \ _ (ツ) _ / ¯ something changed

--- task2/filebeat/do_work.sh
+++ task2/filebeat/do_work.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 service apache2 start
-curl localhost # to fill log file
+curl localhost 1>/dev/null 2>/dev/null # to fill log file
+sleep 80
 /usr/share/filebeat/bin/filebeat -e -c /etc/filebeat/filebeat.yml -path.home /usr/share/filebeat -path.config /etc/filebeat -path.data /var/lib/filebeat -path.logs /var/log/filebeat
