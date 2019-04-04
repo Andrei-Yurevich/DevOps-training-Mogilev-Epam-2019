712c011247989a6927683180ec50c6e6070bb3bc
Andrey1913 <yurevich.andreyszcmavo12@gmail.com>
2019-04-04 03:41:11 -0700

    fixed run_in_background.ps1 script

--- run_in_background.ps1
+++ run_in_background.ps1
@@ -1,4 +1,4 @@
-$publish_folder='C:\Program Files (x86)\Jenkins\workspace\build_asp\samples\MusicStore\bin\Debug\netcoreapp2.0\'
+$publish_folder='C:\Program Files (x86)\Jenkins\workspace\build_asp\samples\MusicStore\bin\Debug\netcoreapp2.0\publish'
 
 cd $publish_folder
 DOTNET.exe MusicStore.dll
